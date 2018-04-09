7d5e8e7e6c015fa5fd6f68b486f23a21fe38e9e5
Venan24 <venanosmic24@gmail.com>
2018-04-09 22:28:31 +0200

    Viber API integration

--- rest/v1/index.php
+++ rest/v1/index.php
@@ -161,4 +161,58 @@ Flight::route('GET /user/@email', function ($email) {
     }
 });
 
+Flight::route('GET|POST /new_webhook_page', function(){
+
+$request = file_get_contents("php://input");
+$file = "file.txt";
+file_put_contents($file, $request, FILE_APPEND | LOCK_EX);
+$input = json_decode($request, true);
+
+if($input['event'] == 'webhook') {
+  $webhook_response['status']=0;
+  $webhook_response['status_message']="ok";
+  $webhook_response['event_types']='delivered';
+  echo json_encode($webhook_response);
+  die;
+}
+else if($input['event'] == "subscribed") {
+  // when a user subscribes to the public account
+}
+else if($input['event'] == "conversation_started"){
+  // when a conversation is started
+}
+else if($input['event'] == "message") {
+  /* when a user message is received */
+  $type = $input['message']['type']; //type of message received (text/picture)
+  $text = $input['message']['text']; //actual message the user has sent
+  $sender_id = $input['sender']['id']; //unique viber id of user who sent the message
+  $sender_name = $input['sender']['name']; //name of the user who sent the message
+
+  // here goes the data to send message back to the user
+  $data['auth_token'] = "47aae3f0eb27d575-ceccfc2bcc198821-18a39192cfe80625";
+  $data['receiver'] = $sender_id;
+  $data['text'] = "Server responded to you: ".$sender_name.".";
+  $data['type'] = 'text';
+
+  //here goes the curl to send data to user
+  $ch = curl_init("https://chatapi.viber.com/pa/send_message");
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  $result = curl_exec($ch);
+}
+
+});
+
+Flight::route('GET|POST /set_webhook', function(){
+$url = 'https://chatapi.viber.com/pa/set_webhook';
+$jsonData='{ "auth_token": "47aae3f0eb27d575-ceccfc2bcc198821-18a39192cfe80625", "url": "https://monitor.biznet.ba/rest/v1/new_webhook_page" }';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+$result = curl_exec($ch);
+curl_close($ch);
+});
+
 Flight::start();
